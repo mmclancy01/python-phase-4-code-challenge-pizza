cd1a72ec839c16da1a4afa98efd46eb36479cd74
Matthew Clancy <mmclancy01@yahoo.com>
2024-04-09 10:35:27 -0400

    finished code challenge

--- server/app.py
+++ server/app.py
@@ -24,6 +24,50 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.get("/restaurants")
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return [r.to_dict(rules= ['-restaurant_pizzas']) for r in restaurants], 200
+
+@app.get("/restaurants/<int:id>")
+def get_rest_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if not restaurant:
+        return {"error": "Restaurant not found"}, 404
+    else: 
+        return restaurant.to_dict(), 200
+    
+@app.delete("/restaurants/<int:id>")
+def delete_rest(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if not restaurant:
+        return {"error": "Restaurant not found"}, 404
+    db.session.delete(restaurant)
+    db.session.commit()
+    return {}, 204
+
+@app.get("/pizzas")
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return [p.to_dict(rules= ['-restaurant_pizzas']) for p in pizzas], 200
+
+@app.post("/restaurant_pizzas")
+def post_pizza():
+    json_data = request.get_json()
+
+    try:
+        new_pizza = RestaurantPizza(
+            price = json_data.get('price'),
+            pizza_id = json_data.get('pizza_id'),
+            restaurant_id = json_data.get('restaurant_id')
+        )
+    except ValueError as e:
+        return {"errors": ["validation errors"]}, 400
+    
+    db.session.add(new_pizza)
+    db.session.commit()
+
+    return new_pizza.to_dict(), 201
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
